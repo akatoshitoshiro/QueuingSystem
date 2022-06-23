000b464e900629763198ed2c508ecedf7c8407e8
Vu <akatoshitoshiro@gmail.com>
2022-06-23 11:08:39 +0700

    23/06/2022

--- src/Components/Modal/Modal.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './modal.scss';
-const Modal: React.FC<{ children: JSX.Element }> = ({ children }) => {
-  return <div className="app__modal">{children}</div>;
-};
-
-export default Modal;
